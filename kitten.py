1760c39b87c7dd949f2f5f75add6884a16a07855
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2023-04-08 12:58:00 +0200

    Implemented explode and defuse without putting back

--- kitten.py
+++ kitten.py
@@ -1,8 +1,12 @@
 def winner(player1: str, player2: str, player1_defuse: int, player2_defuse: int, cards: list[str]) -> str:
-    if len(cards) == 2:
-        if cards[0] == "defuse":
-            return player2
-        return player1
-    if player1_defuse > player2_defuse:
+    if player1_defuse + leading_refuses(cards) % 2 > player2_defuse:
         return player1
     return player2
+
+
+def leading_refuses(cards: list[str]) -> int:
+    if len(cards) == 0:
+        return 0
+    if "explode" not in cards:
+        return len(cards)
+    return len(cards) - cards.index("explode") - 1
---
+++ test/test_explode_endgame.py
@@ -0,0 +1,31 @@
+from kitten import winner
+
+
+def test_one_defuse_in_stack_explode_last():
+    assert winner("Kamil", "Daniel", 0, 0, ["explode", "defuse"]) == "Kamil"
+    assert winner("Daniel", "Kamil", 0, 0, ["explode", "defuse"]) == "Daniel"
+
+
+def test_one_defuse_in_stack_explode_first():
+    assert winner("Kamil", "Daniel", 0, 0, ["defuse", "explode"]) == "Daniel"
+    assert winner("Daniel", "Kamil", 0, 0, ["defuse", "explode"]) == "Kamil"
+
+
+def test_two_defuses_in_stack_explode_last():
+    assert winner("Kamil", "Daniel", 0, 0, ["explode", "defuse", "defuse"]) == "Daniel"
+    assert winner("Daniel", "Kamil", 0, 0, ["explode", "defuse", "defuse"]) == "Kamil"
+
+
+def test_two_defuses_in_stack_explode_middle():
+    assert winner("Kamil", "Daniel", 0, 0, ["defuse", "explode", "defuse"]) == "Kamil"
+    assert winner("Daniel", "Kamil", 0, 0, ["defuse", "explode", "defuse"]) == "Daniel"
+
+
+def test_two_defuses_in_stack_explode_first():
+    assert winner("Kamil", "Daniel", 0, 0, ["defuse", "defuse", "explode"]) == "Daniel"
+    assert winner("Daniel", "Kamil", 0, 0, ["defuse", "defuse", "explode"]) == "Kamil"
+
+
+def test_initial_defuse_and_defuse_from_cards():
+    assert winner("Kamil", "Daniel", 0, 2, ["explode", "defuse"]) == "Daniel"
+    assert winner("Daniel", "Kamil", 0, 2, ["explode", "defuse"]) == "Kamil"
