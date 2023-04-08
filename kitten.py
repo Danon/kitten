81a164afafd7474e57fdf9961359a060ff52f4ad
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2023-04-08 11:35:41 +0200

    Implemented stack of two cards

--- kitten.py
+++ kitten.py
@@ -1,4 +1,8 @@
-def winner(player1: str, player2: str, player1_defuse: int, player2_defuse: int) -> str:
+def winner(player1: str, player2: str, player1_defuse: int, player2_defuse: int, cards: list[str]) -> str:
+    if len(cards) == 2:
+        if cards[0] == "defuse":
+            return player2
+        return player1
     if player1_defuse > player2_defuse:
         return player1
     return player2
