f19c7f32018f1193b3aa23c3c55d6f51b279d7e2
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2023-04-08 11:03:46 +0200

    Added winner()

--- test_kitten.py
+++ test_kitten.py
@@ -1,2 +1,7 @@
+def winner(player1: str, player2: str) -> str:
+    return player2
+
+
 def test():
-    assert True
+    assert winner("Kamil", "Daniel") == "Daniel"
+    assert winner("Daniel", "Kamil") == "Kamil"
